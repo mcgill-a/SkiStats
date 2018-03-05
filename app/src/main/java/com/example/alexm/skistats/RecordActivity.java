66b36ed569be37010bc45631c01e984c9062e630
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2018-03-05 21:12:05 +0000

    Added recording buttons

--- app/src/main/java/com/example/alexm/skistats/RecordActivity.java
+++ app/src/main/java/com/example/alexm/skistats/RecordActivity.java
@@ -1,13 +1,63 @@
 package com.example.alexm.skistats;
 
+import android.icu.text.AlphabeticIndex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class RecordActivity extends AppCompatActivity {
+
+
+    private ImageButton recordImageButton;
+    private ImageButton pauseImageButton;
+    private ImageButton cancelImageButton;
+    private ImageButton submitImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+
+        recordImageButton = (ImageButton)findViewById(R.id.recordImageButton);
+        pauseImageButton = (ImageButton)findViewById(R.id.pauseImageButton);
+        cancelImageButton = (ImageButton)findViewById(R.id.cancelImageButton);
+        submitImageButton = (ImageButton)findViewById(R.id.submitImageButton);
+
+        recordImageButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Toast.makeText(RecordActivity.this, "Recording Started", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        pauseImageButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Toast.makeText(RecordActivity.this, "Paused Recording", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cancelImageButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Toast.makeText(RecordActivity.this, "Cancelled Recording", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        submitImageButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Toast.makeText(RecordActivity.this, "Submitted Recording", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
